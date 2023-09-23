32d7561c3a4c0b6252538dd0c9a4fd720baeefdf
Lorenze Jay <lorenzejaytech@gmail.com>
2023-09-23 15:06:28 -0700

    added tracing macos platform+version

---
+++ lib/utils/traceMacVersion.ts
@@ -0,0 +1,42 @@
+
+import * as os from 'os';
+import 'dotenv/config'
+import Airtable from 'airtable'
+
+
+
+const apiKey = process.env.AIRTABLE_API_KEY;
+const BASE_ID = process.env.BASE_ID;
+
+
+const base = new Airtable({apiKey}).base(BASE_ID);
+
+async function logSystemInfo() {
+    const osType = os.type();
+    const osPlatform = os.platform();
+    const osRelease = os.release();
+  console.log({
+    'OS Type': osType,
+    'OS Platform': osPlatform,
+    'OS Release': osRelease,
+
+})
+
+    try {
+      await base('Table 1').create([
+        {
+            fields: {
+                'OS Type': osType,
+                'OS Platform': osPlatform,
+                'OS Release': osRelease,
+
+            }
+        }
+    ])
+  
+    } catch (error) {
+        console.error('Error logging system info to Airtable:', error);
+    }
+}
+
+logSystemInfo();
